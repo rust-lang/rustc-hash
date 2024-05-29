5767174a2a2ee530f00a019754da4b4d5188f7a5
xermicus <cyrill@parity.io>
2024-05-29 10:12:16 +0200

    make FxSeededState no_std compatible
    Signed-off-by: xermicus <cyrill@parity.io>

--- src/seeded_state.rs
+++ src/seeded_state.rs
@@ -1,11 +1,14 @@
+#[cfg(feature = "std")]
 use std::collections::{HashMap, HashSet};
 
 use crate::FxHasher;
 
 /// Type alias for a hashmap using the `fx` hash algorithm with [`FxSeededState`].
+#[cfg(feature = "std")]
 pub type FxHashMapSeed<K, V> = HashMap<K, V, FxSeededState>;
 
 /// Type alias for a hashmap using the `fx` hash algorithm with [`FxSeededState`].
+#[cfg(feature = "std")]
 pub type FxHashSetSeed<V> = HashSet<V, FxSeededState>;
 
 /// [`FxSetState`] is an alternative state for `HashMap` types, allowing to use [`FxHasher`] with a set seed.
@@ -41,16 +44,13 @@ impl core::hash::BuildHasher for FxSeededState {
 mod tests {
     use core::hash::BuildHasher;
 
-    use crate::{FxHashMapSeed, FxSeededState};
+    use crate::FxSeededState;
 
     #[test]
     fn different_states_are_different() {
-        let a = FxHashMapSeed::<&str, u32>::with_hasher(FxSeededState::with_seed(1));
-        let b = FxHashMapSeed::<&str, u32>::with_hasher(FxSeededState::with_seed(2));
+        let a = FxSeededState::with_seed(1);
+        let b = FxSeededState::with_seed(2);
 
-        assert_ne!(
-            a.hasher().build_hasher().hash,
-            b.hasher().build_hasher().hash
-        );
+        assert_ne!(a.build_hasher().hash, b.build_hasher().hash);
     }
 }
