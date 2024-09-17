cc979ed2bb65c75c7c656afda56c3fafcb39402d
Allan <6740989+allan2@users.noreply.github.com>
2024-09-16 22:47:36 -0400

    Remove nursery lint group

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![warn(clippy::all, clippy::pedantic, clippy::nursery, clippy::cargo)]
+#![warn(clippy::all, clippy::pedantic, clippy::cargo)]
 #![allow(
     clippy::unreadable_literal,
     clippy::must_use_candidate,
